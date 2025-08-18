2c2fbfc3bdbadbccb2caaf6e411837f48dab826d
Isak Näslund <isak.naslund@burohappold.com>
2025-08-18 10:55:56 +0100

    Try grab branch name from args

--- .ci/Generation/SchemaGeneration/SchemaGeneration/Program.cs
+++ .ci/Generation/SchemaGeneration/SchemaGeneration/Program.cs
@@ -12,14 +12,29 @@ Generate.Path = rootFolder; // Set the path for schema generation
 Console.WriteLine("Loading BHoM assemblies...");
 List<Assembly> assemblies = AssemblyLoader.LoadAlloMAssemblies(new List<string> { "BHoM" });
 
+//Set branch for config to be used for schema IDs
+string branch;
+if(args.Length > 0)
+{
+    branch = args[0];
+}
+else
+{
+    branch = "develop"; // Default branch if none is provided
+}
+
+Console.WriteLine($"Using branch: {branch} for schema IDs.");
+
+//Set up config for schema generation
 ConvertConfig config = new ConvertConfig
 {
     IncludeId = true,
     IncludeInnerIds = false,
     TypesAsRef = true,
-    Branch = "develop"
+    Branch = branch
 };
 
+// Generate schemas for each assembly
 foreach (Assembly assembly in assemblies)
 {
     Console.WriteLine($"Generating schema for {assembly.FullName}...");
